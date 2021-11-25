a2877590f92ed82b619868ccb28aec00a0f5e8dd
kaiwenli1 <72473351+RealKaiwenLi@users.noreply.github.com>
2021-11-24 22:37:56 -0800

    user and asset table created

--- agmonitor/users/models.py
+++ agmonitor/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db.models.fields import EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.db import models
 
 
 class User(AbstractUser):
@@ -20,3 +22,17 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+class ag_user(models.Model):
+    user_email = models.EmailField(max_length=100, unique=True, primary_key=True)
+    
+    def __str__(self):
+        return self.user_email
+
+class ag_user_asset(models.Model):
+    user = models.ForeignKey(ag_user, on_delete=models.DO_NOTHING)
+    asset_name = models.CharField(max_length=100)
+    description = models.TextField(max_length=254 ,blank=True)
+
+    def __str__(self):
+        return self.asset_name
