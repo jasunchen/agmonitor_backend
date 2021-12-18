61290d9f4314b227275b28cbb436ba50ef393909
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2021-12-17 17:06:13 -0800

    bug

--- ucsb/repository/asset_data_repository.py
+++ ucsb/repository/asset_data_repository.py
@@ -1,11 +1,30 @@
-from ucsb.models import asset_data
+from ucsb.models import user_asset, asset_data
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+import datetime
 
-def create_asset_data(start_time, interval, asset_id, consumed_energy, produced_energy):
-    res = ""
-    try:
-        asset_data_obj = asset_data(start_time=start_time, interval=interval, asset_id=asset_id, consumed_energy=consumed_energy, produced_energy=produced_energy)
-        asset_data_obj.save()
-        res = "Asset data created successfully"
-    except Exception as e:
-        res = "Error: " + str(e)
-    return res
+@api_view(['POST'])
+def create_asset_data(request):
+    id = request.data.get('id')
+    tmp_asset = user_asset.objects.get(id=id)
+    data = request.data.get('data')
+    res = []
+    res.append(model_to_dict(tmp_asset))
+    for d in data:
+        date = datetime(2015, 10, 9, 23, 55, 59, 342380)
+        tmp_data = asset_data(asset_id=tmp_asset, start_time=date, interval=d['interval'], consumed_energy=d['consumed_energy'], produced_energy=d['produced_energy'])
+        # tmp_data.save()
+        result = model_to_dict(tmp_data)
+        res.append(result)
+
+    return Response(res)
+
+
+
+def delete_asset_data(id):
+    tmp_asset = user_asset.objects.get(id=id)
+    data_set = asset_data.objects.filter(asset_id=tmp_asset)
+    for d in data_set:
+        d.delete()
+    return True
--- ucsb/repository/asset_repository.py
+++ ucsb/repository/asset_repository.py
@@ -1,11 +1,23 @@
-from ucsb.models import user_asset
+from ucsb.models import user_asset, user
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
 
-def create_asset(user, name, desc):
-    res = ""
-    try:
-        asset = user_asset.UserAsset(user, name, desc)
-        asset.save()
-        res = "Asset created successfully"
-    except Exception as e:
-        res = "Error: " + str(e)
-    return res
+@api_view(['POST', 'DELETE'])
+def create_asset(request):
+    email = request.data.get('email')
+    tmp_user = user(user_email=email)
+    name = request.data.get('name')
+    desc = request.data.get('desc')
+    asset = user_asset(user=tmp_user, asset_name=name, description=desc)
+    asset.save()
+    return Response("Asset created successfully")
+
+#test function
+@api_view(['GET'])
+def getAllAssets(request):
+    res = []
+    result = user_asset.objects.all()
+    for r in result:
+        res.append(model_to_dict(r))
+    return Response(res)
