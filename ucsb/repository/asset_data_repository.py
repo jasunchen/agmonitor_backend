9b0cacca885847b7d0c1c5655ff8cee717f4c737
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2021-12-18 17:32:43 -0800

    create_asset_data succeed

--- ucsb/repository/asset_data_repository.py
+++ ucsb/repository/asset_data_repository.py
@@ -10,22 +10,21 @@ def create_asset_data(request):
     tmp_asset = user_asset.objects.get(id=id)
     data = request.data.get('data')
     res = []
+    asset_data_start_time = asset_data.objects.filter(asset_id=tmp_asset).values_list('start_time', flat=True)
     for d in data:
-        res.append(d)
         start_time_str = d['start_time']
         start_time = datetime.strptime(start_time_str, '%Y-%m-%d %H:%M').strftime('%s')
-        tmp_data = asset_data(asset_id=tmp_asset, start_time=start_time, interval=d['interval'], consumed_energy=d['consumed_energy'], produced_energy=d['produced_energy'])
-        tmp_data.save()
-        result = model_to_dict(tmp_data)
-        res.append(result)
-    return Response({'detail': "Data created successfully"}, status=200)
+        if start_time not in asset_data_start_time:
+            tmp_data = asset_data(asset_id=tmp_asset, start_time=start_time, interval=d['interval'], consumed_energy=d['consumed_energy'], produced_energy=d['produced_energy'])
+            tmp_data.save()
+    return Response({"detail": "Data created successfully"}, status=200)
 
 
 @api_view(['DELETE'])
 def deleteAssetData(request):
     id = request.data.get('id')
     delete_asset_data(id)
-    return Response({'detail': "Data deleted successfully"}, status=200)
+    return Response({"detail": "Data deleted successfully"}, status=200)
 
 
 
