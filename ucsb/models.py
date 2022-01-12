7a03dd5ac3a4ff10b6861989812d7b58773118d1
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-01-11 22:29:48 -0800

    database AI configuration

--- ucsb/models.py
+++ ucsb/models.py
@@ -3,7 +3,14 @@ from datetime import datetime
 
 class user(models.Model):
     user_email = models.EmailField(max_length=100, unique=True, primary_key=True)
-    
+    low_limit = models.IntegerField(default=0)
+    max_limit = models.IntegerField(default=100)
+    battery_size = models.IntegerField(default=0)
+    cost_or_shutoff = models.IntegerField(default=50)
+    hours_of_power = models.IntegerField(default=0)
+    longitude = models.FloatField(default=34.4208)
+    latitude = models.FloatField(default=-119.6982)
+
     def __str__(self):
         return self.user_email
 
@@ -11,6 +18,9 @@ class user_asset(models.Model):
     user = models.ForeignKey(user, on_delete=models.DO_NOTHING)
     description = models.TextField(max_length=254 ,blank=True)
     asset_name = models.CharField(max_length=254, blank=True)
+    declination = models.FloatField(default=0 ,blank=True)
+    azimuth = models.FloatField(default=0 ,blank=True)
+    modules_power = models.FloatField(default=0 ,blank=True)
 
     def __str__(self):
         return self.asset_name
