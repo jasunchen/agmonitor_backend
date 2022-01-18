c7b0d48e888e4ace956fc17006c3b746bf51fcd8
Jasun Chen <chen.jasun@gmail.com>
2022-01-18 13:44:14 -0800

    small mod to mvp func

--- opt/optimization.py
+++ opt/optimization.py
@@ -6,20 +6,16 @@ import numpy as np
 def find_optimal_threshold(weight1, weight2, lowerLimit, maximumLimit, shutOffRisk, idealReserveThreshold):
     #obj = [weight1, weight2]
     availableRange = maximumLimit-lowerLimit
-
-    lhs_ineq = [[-1, 0]]
-    rhs_ineq = [-lowerLimit]
-
-    cost = weight1*(1/(availableRange)) - weight2*(shutOffRisk/idealReserveThreshold)
+    cost = weight1*(1/(availableRange)) + weight2*( - shutOffRisk/idealReserveThreshold)
 
     obj = [cost]
     lhs_ineq = [[ -1], 
             [1]]  
 
     rhs_ineq = [-lowerLimit,  
-            maximumLimit]  
-    # lhs_eq = [[-1]]     # eq constraint left side
-    # rhs_eq = [15]       # eq constraint right side
+            idealReserveThreshold]  
+    lhs_eq = [[0]]     # eq constraint left side
+    rhs_eq = [ weight2*(shutOffRisk) - weight1*(lowerLimit/availableRange)]       # eq constraint right side
 
     bnd = [(0, maximumLimit)]  # Bounds of x
     opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd, method="revised simplex")
@@ -29,10 +25,10 @@ def find_optimal_threshold(weight1, weight2, lowerLimit, maximumLimit, shutOffRi
 
 
 if __name__ == "__main__":
-    weight1 = 0.5
-    weight2 = 0.5
+    weight1 = 0.4
+    weight2 = 0.6
     lowerLimit = 10
     maximumLimit = 90
-    shutOffRisk = 1
+    shutOffRisk = 0.9
     idealReserveThreshold = 80
     print(find_optimal_threshold(weight1, weight2, lowerLimit, maximumLimit, shutOffRisk,idealReserveThreshold))
