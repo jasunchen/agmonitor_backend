f4fe04df8386e35028d9f51f9158b0a2eebf4722
Jasun Chen <chen.jasun@gmail.com>
2022-01-20 10:32:46 -0800

    shutoff and idealReserve calcs

--- opt/optimization.py
+++ opt/optimization.py
@@ -2,6 +2,20 @@ from scipy.optimize import linprog
 import numpy as np
 
 
+# alerts: an array severity rankings
+def calculate_shutOffRisk(alerts):
+    risk = 0
+    for alert in alerts:
+        if alert == 1 or alert == "Advisory":
+            risk += 0.1
+        elif alert == 2 or alert == "Watch":
+            risk += 0.34
+        elif alert == 3 or alert == "Warning":
+            risk += 0.67
+    return min(1, risk)
+
+def calculate_idealReserveThreshold(numberOfHours, avgBaseload, batterySize):
+    return min(1,(numberOfHours * avgBaseload) / batterySize)
 
 def find_optimal_threshold(weight1, weight2, lowerLimit, maximumLimit, shutOffRisk, idealReserveThreshold):
     #obj = [weight1, weight2]
@@ -31,4 +45,5 @@ if __name__ == "__main__":
     maximumLimit = 90
     shutOffRisk = 0.9
     idealReserveThreshold = 80
+    print(calculate_shutOffRisk([1,2,"Warning"]))
     print(find_optimal_threshold(weight1, weight2, lowerLimit, maximumLimit, shutOffRisk,idealReserveThreshold))
