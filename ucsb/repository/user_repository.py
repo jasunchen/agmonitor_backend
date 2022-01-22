3d1f5600ec2db6c6e7dc02d373e7aa0cdd7292ee
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-01-21 19:35:33 -0800

    api initialize

--- ucsb/repository/user_repository.py
+++ ucsb/repository/user_repository.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from django.forms.models import model_to_dict
 from rest_framework.decorators import api_view
 from ucsb.repository.helpers import *
+from opt.optimization import *
+from opt.utility.weather import *
 
 
 @api_view(['POST', 'DELETE'])
@@ -99,3 +101,35 @@ def register_user(request):
            return Response({"detail": "User created successfully"}, status=200)
     else:
         return Response({"detail": "Error: Invalid request"}, status=400)
+
+
+@api_view(['POST'])
+def calculate(request):
+    params = ["email"]
+    #Check for Required Fields
+    for p in params:
+        if request.query_params.get(p, None) == None:
+            return Response(
+                {"message": "Missing Required Parameters: {}".format(p)}, 
+                status = 400)
+
+    #Check for Invalid Parameters
+    if verify(request.query_params, params): 
+        return Response(
+            {"message": "Request has invalid parameter not in {}".format(params)}, 
+            status = 400)
+
+    email = request.query_params.get('email')
+    tmp_user = user.objects.get(user_email=email)
+
+    low_limit = tmp_user.low_limit
+    max_limit = tmp_user.max_limit
+    battery_size = tmp_user.battery_size
+    cost_or_shutoff = tmp_user.cost_or_shutoff
+    hours_of_power = tmp_user.hours_of_power
+    longitude = tmp_user.longitude
+    latitude = tmp_user.latitude
+    alert = get_alerts(latitude, longitude)
+    risk = calculate_shutOffRisk(alert)
+
+
