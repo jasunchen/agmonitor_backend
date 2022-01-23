4e66cc261a1286b38ba1ba587331fd8b8c246c59
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-01-23 00:02:16 -0800

    remove docker ci command

--- opt/utility/email.py
+++ opt/utility/email.py
@@ -1,17 +1,19 @@
+from email import message
 import smtplib, ssl
 
 def send_email(sender, password, receiver):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    message = """\
+    messages = """\
     Subject: Hi there
     This message is sent from Python."""
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender, password)
-        server.sendmail(sender, receiver, message)
-        print("success")
+        server.sendmail(sender, receiver, messages)
+        print("Email sent successfully")
+
 
 if __name__ == "__main__":
     sender = "yuyuanwang1999@gmail.com"
