63bba0129d19a21af01c19fb43e0088e1d6a8072
alexmeigz <alexmei@ucsb.edu>
2022-01-24 22:16:50 -0800

    solar array in correct format

--- opt/utility/solar.py
+++ opt/utility/solar.py
@@ -1,4 +1,8 @@
 import requests
+from datetime import datetime
+
+def convertTime(s):
+    return int((datetime.strptime(s, '%Y-%m-%d %H:%M:%S') - datetime(1970,1,1)).total_seconds())
 
 def getSolarData(latitude: float, longitude: float, declination: float, azimuth: float, power: float):
     response = requests.get(
@@ -8,7 +12,30 @@ def getSolarData(latitude: float, longitude: float, declination: float, azimuth:
     response = response.json()
 
     if response['message']['code'] == 0:
-        return (200, [[k, v] for k, v in response['result']['watt_hours'].items()])
+        result = [[15 * t, 0] for t in range(192)]
+        data = [[convertTime(k), v] for k, v in response['result']['watt_hours'].items()]
+        
+        # offset for odd / even days
+        offset = 86400 if data[0][0] % 172800 >= 86400 else 0
+        previousIndex = 0
+        previousValue = 0
+
+        for t, v in data:
+            # calculate index of result array
+            index = int((t + offset) % 172800 / 60 // 15)
+
+            # calculate energy generated
+            energyGenerated = max(0, v - previousValue)
+            
+            # calculate energy generated between index and previousIndex
+            # assume energy generated equally through time period (this is not great, but workable)
+            for i in range(previousIndex, index):
+                result[i][1] += energyGenerated / (index - previousIndex)
+            
+            previousValue = v
+            previousIndex = index
+
+        return (200, result)
     else:
         return (400, response['message']['text'])
 
