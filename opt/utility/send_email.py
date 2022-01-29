d36aeac1d7903a7744fa42065f545d830e49b309
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-01-29 11:21:39 -0800

    email

--- opt/utility/send_email.py
+++ opt/utility/send_email.py
@@ -1,21 +1,27 @@
 import smtplib, ssl
+from email.message import EmailMessage
 
-def send_email(receiver):
+def send_email(receiver, message):
     sender = "yuyuanwang1999@gmail.com"
     password = "ytpuqhpomlekpeqh"
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    messages = """\
-    Subject: Hi there
-    This message is sent from Python."""
-    
     context = ssl.create_default_context()
+    msg = EmailMessage()
+    msg.set_content(message)
+
+    msg['Subject'] = 'Agmnitor Notification'
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # Send the message via our own SMTP server.
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender, password)
-        server.sendmail(sender, receiver, messages)
+        server.send_message(msg)
 
 
 if __name__ == "__main__":
     
-    receiver = "alexmei@ucsb.edu"
-    send_email(receiver)
+    receiver = "kaiwen_li@ucsb.edu"
+    message = "Hello, this is a test email"
+    send_email(receiver, message)
