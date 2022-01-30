cbf105bb3bb6eec5d38c4ff4239ba3132cd80601
Jasun Chen <chen.jasun@gmail.com>
2022-01-29 23:10:42 -0800

    added message

--- opt/utility/scheduler.py
+++ opt/utility/scheduler.py
@@ -1,15 +1,19 @@
+from torch import threshold
 from opt.utility.send_email import send_email
 # from ucsb.models import *
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from pytz import utc
 
+message = "Hello, this is a daily automated notification. Based on weather forecasts and historical data for tomorrow, the ideal reserve percentage for your battery is {} percent. Please visit https://agmonitor-pina-colada.herokuapp.com/ for more details."
+
 def opt_scheduler():
     from ucsb.models import user
     users = user.objects.all()
     for user in users:
         receiver = user.user_email
-        message = "Hello, this is a test email"
+        threshold = 50 #todo
+        message.format(threshold)
         send_email(receiver, message)
 
 def start():
