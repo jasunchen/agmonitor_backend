d11b963385b29d9900f599f7958441985e4d26c8
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-02-02 23:06:08 -0800

    Update send_message.py

--- opt/utility/send_message.py
+++ opt/utility/send_message.py
@@ -2,12 +2,12 @@ from twilio.rest import Client
 
 
 def send_message(mes, phone_number):
-    account = "ACb7aa0b75ba44f21340d4ddf870cfac3a"
-    token = "1b336073000ea39f2abae91c961d5c1b"
+    account = "account"
+    token = "token"
     client = Client(account, token)
     to_number = '+1' + phone_number
 
-    message = client.messages.create(to=to_number, from_="+19034597869", body=mes)
+    message = client.messages.create(to=to_number, from_="number", body=mes)
 
 if __name__ == "__main__":
     send_message("Hello", "6263619710")
