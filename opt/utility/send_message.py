adff819499e293a7ca3ccd22a671c744a608dff6
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-02-02 23:12:22 -0800

    token security added

--- opt/utility/send_message.py
+++ opt/utility/send_message.py
@@ -1,13 +1,17 @@
 from twilio.rest import Client
+import environ
+
+env = environ.Env()
 
 
 def send_message(mes, phone_number):
-    account = "account"
-    token = "token"
+    account = env('ACCOUNT')
+    token = env('TOKEN')
     client = Client(account, token)
     to_number = '+1' + phone_number
+    from_number = env('PHONE_NUMBER')
 
-    message = client.messages.create(to=to_number, from_="number", body=mes)
+    message = client.messages.create(to=to_number, from_=from_number, body=mes)
 
 if __name__ == "__main__":
     send_message("Hello", "6263619710")
