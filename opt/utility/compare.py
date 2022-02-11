e684f6421bea5f5cb0dfd0dae50d5f714e1d5ff0
Jasun Chen <chen.jasun@gmail.com>
2022-02-10 21:43:11 -0800

    added lin fit and applying fit

--- opt/utility/compare.py
+++ opt/utility/compare.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import environ
+import numpy as np
 
 env = environ.Env()
 url = "https://api.enphaseenergy.com/api/v2/systems/2140366/stats?start_at={}&end_at={}&key=f2af1b02da727c9f4f06bf4a80e2f3ba&user_id=4d6a49784f446b784d413d3d0a"
@@ -18,6 +19,21 @@ def get_data(start_time):
 
     return arr
 
+def linear_fit(real, predicted):
+    real_0, pred_0 = real[0], predicted[0]
+    real_max = max(real)
+    pred_max = max(predicted)
+    m = (pred_max-pred_0)/(real_max-real_0)
+    b = pred_0 - m * real_0
+    return m, b
+
+def apply_fit(m,b, predicted):
+    return [(i - b)/m for i in predicted]
+
 if __name__ == "__main__":
     start_time = 1644393600
     actual_generated = get_data(start_time)
+    print(actual_generated)
+    print(len(actual_generated))
+    #print(linear_fit(actual_generated, [0]*96))
+    print(linear_fit([0,1], [0,1]))
